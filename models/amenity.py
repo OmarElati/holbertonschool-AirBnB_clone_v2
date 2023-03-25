0c27d8b93ac68447e621023f066ffe4c8d30925d
OmarElati <5796@holbertonstudents.com>
2023-03-25 11:25:20 +0100

    test

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Table, ForeignKey
 
 
-class Amenity(BaseModel):
-    name = ""
+# Define the place_amenity table
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
+
+
+# Define the Place table
+class Place(Base):
+    __tablename__ = 'places'
+    id = Column(String(60), primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+
+
+# Define the Amenity table
+class Amenity(Base):
+    __tablename__ = 'amenities'
+    id = Column(String(60), primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+class Amenity(BaseModel, Base):
+     __tablename__ = 'amenities'
+     name = Column(String(128), nullable=False)
+     place_amenities = relationship("Place", secondary=place_amenity)
--- models/place.py
+++ models/place.py
@@ -1,15 +1,59 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, Table, ForeignKey
 from sqlalchemy.orm import relationship
 import models
 from models import *
+from models.amenity import Amenity
+
+
+
+# Define the place_amenity table
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
+
+
+# Define the Place table
+class Place(Base):
+    __tablename__ = 'places'
+    id = Column(String(60), primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+
+
+# Define the Amenity table
+class Amenity(Base):
+    __tablename__ = 'amenities'
+    id = Column(String(60), primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+storage_type = "string"
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
+
+    if storage_type == 'db':
+        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+    else:
+        @property
+        def amenities(self):
+            amenities_list = []
+            for amenity_id in self.amenity_ids:
+                amenity = models.storage.get(Amenity, amenity_id)
+                if amenity is not None:
+                    amenities_list.append(amenity)
+            return amenities_list
+
+        @amenities.setter
+        def amenities(self, obj):
+            if isinstance(obj, Amenity):
+                if obj.id not in self.amenity_ids:
+                    self.amenity_ids.append(obj.id)
 
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
