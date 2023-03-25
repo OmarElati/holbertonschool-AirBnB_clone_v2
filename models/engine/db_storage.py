6f403f4f080d59876aa17f259ae2d1f53b1dd1fa
OmarElati <5796@holbertonstudents.com>
2023-03-25 08:27:48 +0100

    test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,7 +5,7 @@ This module defines the DBStorage class
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import BaseModel, Base
+from models.base_model import Base
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -31,13 +31,14 @@ class DBStorage:
 
     def __init__(self):
         """Create the engine and links it to the MySQL database and user."""
-        self.__engine = create_engine(
-            'mysql+mysqldb://{}:{}@{}/{}'
-            .format(os.getenv('HBNB_MYSQL_USER'),
-                    os.getenv('HBNB_MYSQL_PWD'),
-                    os.getenv('HBNB_MYSQL_HOST'),
-                    os.getenv('HBNB_MYSQL_DB')),
-            pool_pre_ping=True)
+        user = os.getenv('HBNB_MYSQL_USER')
+        pwd = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST', 'localhost')
+        db = os.getenv('HBNB_MYSQL_DB')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'
+                                      .format(user, pwd, host, db),
+                                      pool_pre_ping=True)
+
         if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
@@ -45,28 +46,28 @@ class DBStorage:
         """
         Returns a dictionary of instances/objects.
         """
-        if cls is not None:
-            if cls in self.classes:
-                objs = self.__session.query(self.classes[cls]).all()
-                return {obj.__class__.__name__ + '.' + obj.
-                        id: obj for obj in objs}
-            else:
-                return {}
+        from models import classes
+        objects = {}
+        if cls:
+            if type(cls) == str:
+                cls = classes[cls]
+            query = self.__session.query(cls).all()
+            for obj in query:
+                key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+                objects[key] = obj
         else:
-            obj_dict = {}
-            for cls in self.classes.values():
-                objs = self.__session.query(cls).all()
-                for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
-                    obj_dict[key] = obj
-            return obj_dict
+            for cls in classes.values():
+                query = self.__session.query(cls).all()
+                for obj in query:
+                    key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+                    objects[key] = obj
+        return objects
 
     def new(self, obj):
         """
         Add the object to the current database session.
         """
-        if obj is not None:
-            self.__session.add(obj)
+        self.__session.add(obj)
 
     def save(self):
         """
@@ -78,7 +79,7 @@ class DBStorage:
         """
         Delete obj from the current database session.
         """
-        if obj is not None:
+        if obj:
             self.__session.delete(obj)
 
     def reload(self):
@@ -89,7 +90,8 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
-        self.__session = scoped_session(session_factory)()
+        Session = scoped_session(session_factory)
+        self.__session = Session()
 
     def close(self):
         """
--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+import models
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """
+    State class for storing state data.
+    """
+
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+
+    if models.storage_type == 'db':
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            """
+            Getter attribute that returns the list of City instances with state_id
+            equals to the current State.id.
+            """
+            cities = models.storage.all('City')
+            return [city for city in cities.values() if city.state_id == self.id]
