0538c1bbdad2bd0e0006f21c84452ededac42ac7
OmarElati <5796@holbertonstudents.com>
2023-04-14 11:34:43 +0100

    test

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
-"""
+"""Module for a script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -9,38 +7,28 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """
-    Remove the current SQLAlchemy Session after each request.
-    """
-    storage.close()
-
-
 @app.route('/states', strict_slashes=False)
-def states_list():
-    """
-    List all State objects present in DBStorage sorted by name (A->Z)
-    """
-    states = storage.all(State).values()
-    states_sorted = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html',
-                           states=states_sorted)
+def states():
+    """Displays an HTML page that lists all State objects present in DBStorage sorted by name"""
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.route('/states/<id>', strict_slashes=False)
-def states_cities(id):
-    """
-    List all City objects linked to a State sorted by name (A->Z)
-    """
+def state_cities(id):
+    """Displays an HTML page that lists all City objects linked to the State with the given id"""
     state = storage.get(State, id)
     if state is None:
-        return render_template('9-states.html', state=None)
+        return render_template('9-not_found.html')
+    cities = sorted(state.cities, key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', state=state, cities=cities)
 
-    cities_sorted = sorted(state.cities, key=lambda city: city.name)
-    return render_template('9-states.html',
-                           state=state, cities=cities_sorted)
+
+@app.teardown_appcontext
+def teardown_db(error):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
