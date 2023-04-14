42045f04ef06b7c417d3be20beaa3aaa13ebc6b5
OmarElati <5796@holbertonstudents.com>
2023-04-14 02:10:27 +0100

    script that starrts a Flask web application

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -27,7 +27,8 @@ def c_text(text):
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    """Displays 'Python' followed by the value of the 'text' variable (default 'is cool')"""
+    """Displays 'Python' followed by the value
+    of the 'text' variable (default 'is cool')"""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
