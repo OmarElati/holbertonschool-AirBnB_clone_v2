a832827aac7539b38a1df0458232998457351e27
OmarElati <5796@holbertonstudents.com>
2023-04-14 11:58:34 +0100

    test

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,18 +10,20 @@ app = Flask(__name__)
 @app.route('/states', strict_slashes=False)
 def states():
     """Displays an HTML page that lists all State objects present in DBStorage sorted by name"""
-    states = sorted(storage.all(State).values(), key=lambda x: x.name)
-    return render_template('9-states.html', states=states)
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.route('/states/<id>', strict_slashes=False)
 def state_cities(id):
     """Displays an HTML page that lists all City objects linked to the State with the given id"""
-    state = storage.get(State, id)
-    if state is None:
-        return render_template('9-states.html')
-    cities = sorted(state.cities, key=lambda x: x.name)
-    return render_template('9-states.html', state=state, cities=cities)
+    states = storage.all(State).values()
+    for state in states:
+        if id == state.id:
+            return render_template('9-states.html',
+                                    state=state, state_cities=state.cities)
+
+    return render_template('9-states.html', not_found=True)
 
 
 @app.teardown_appcontext
