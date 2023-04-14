12d87776e88ff38383aafd86fc39f9b4eefdb6d0
OmarElati <5796@holbertonstudents.com>
2023-04-14 10:48:56 +0100

    test

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Starts a Flask web application """
-
 from models import storage
 from models.state import State
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
@@ -17,9 +17,14 @@ def teardown_appcontext(exception):
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """Displays an HTML page with a list of States and Cities"""
-    states = sorted(list(storage.all(State).values()), key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=states)
+    states = storage.all(State).values()
+    cities = list()
+    for state in states:
+        for city in state.cities:
+            cities.append(city)
 
+    return render_template('8-cities_by_states.html',
+                               states=states, state_cities=cities)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
