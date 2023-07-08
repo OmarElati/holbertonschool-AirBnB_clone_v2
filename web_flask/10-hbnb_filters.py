db8a26aa987180306279a1098d8da8b5ecac91da
OmarElati <5796@holbertonstudents.com>
2023-07-08 15:41:53 +0200

    HBNB filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """ removes current SQLAlchemy sesh """
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """ displays HTML for states w state id """
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    cities = sorted(storage.all(City).values(), key=lambda x: x.name)
+    amenities = sorted(storage.all(Amenity).values(), key=lambda x: x.name)
+    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
